d4c0dd0b9b1950b12f4a4c3a0c8cd5c9b3490309
AlvaroBishop <bishopgamma@gmail.com>
2022-12-07 23:29:34 -0600

    add appBar with search input

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// main.dart
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,111 +6,124 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        // Remove the debug banner
+        debugShowCheckedModeBanner: false,
+        title: 'b-shop',
+       
+        home: const HomePage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
+  
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar(),
+          // Other Sliver Widgets
+          SliverList(
+            delegate: SliverChildListDelegate([
+              const SizedBox(
+                height: 400,
+                child: Center(
+                  child: Text(
+                    'This is an awesome shopping platform',
+                  ),
+                ),
+              ),
+              Container(
+                height: 1000,
+                color: Colors.pink,
+              ),
+            ]),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+
+class _CustomAppBar extends StatelessWidget {
+  
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final searchController = TextEditingController();
+
+    void _search() {
+      print(searchController.text);
+    }
+    return SliverAppBar(
+            
+            expandedHeight: 80,
+            collapsedHeight: 60,
+            floating: true,
+            pinned: false,
+            snap: true,
+            titleTextStyle: TextStyle(height: 100),
+            centerTitle: true,
+            title: AppBar(
+              toolbarHeight: 60,
+              elevation: 0,
+              title: Container(
+
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(70)),
+                ),
+                height: 60,
+                padding: EdgeInsets.only(bottom: 10),
+                
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: searchController,
+                        onChanged: (value) => _search(),
+                        decoration: InputDecoration(
+                          fillColor: Colors.amber,
+                          focusColor: Colors.red,
+                            hintText: 'Search for something',
+                            prefixIcon: Icon(Icons.search),
+                            suffixIcon: IconButton(onPressed: () => searchController.text = '', icon: Icon(Icons.close)),
+                            
+                          ),
+                      ),
+                    ),
+                    Container(
+                      
+                      margin: const EdgeInsets.only(right: 10),
+                      child: CircleAvatar(
+                        maxRadius: 15,
+                        backgroundColor: Colors.green[900],
+                        backgroundImage: NetworkImage('https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            
+            
+            
+          );
   }
 }
