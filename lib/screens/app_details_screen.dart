71b1936644ca907a2364407d18cd4bda2e9cf8b4
AlvaroBishop <bishopgamma@gmail.com>
2023-01-18 15:39:55 -0600

    finish app details screen

--- lib/screens/app_details_screen.dart
+++ lib/screens/app_details_screen.dart
@@ -14,8 +14,6 @@ class AppDetails extends StatefulWidget {
 }
 
 class _AppDetailsState extends State<AppDetails> {
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,76 +26,231 @@ class _AppDetailsState extends State<AppDetails> {
               Container(
                 margin: const EdgeInsets.all(10),
                 height: 100,
-                child: Row(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: const FadeInImage(
-                        placeholder: AssetImage('assets/no-image.jpg'),
-                        image: AssetImage('assets/no-image.jpg'),
-                        width: 80,
-                        height: 80,
-                        fit: BoxFit.cover,
-                      ),
+                child: Row(children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: const FadeInImage(
+                      placeholder: AssetImage('assets/no-image.jpg'),
+                      image: AssetImage('assets/no-image.jpg'),
+                      width: 80,
+                      height: 80,
+                      fit: BoxFit.cover,
                     ),
-                    Container(
-                      margin: const EdgeInsets.all(15),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children:  [
-                          Text("App Name", style: TextStyle(fontSize: 26, color: Colors.white, fontFamily: 'OpenSans'),),
-                          SizedBox(height: 5,),
-                          Text("App Name.com", style: TextStyle(fontSize: 15, color: Colors.teal, fontFamily: 'OpenSans'),),
-                          
-                        ]
-                        ),
-                    )
-                  ]
                   ),
+                  Container(
+                    margin: const EdgeInsets.all(15),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            "App Name",
+                            style: TextStyle(
+                                fontSize: 26,
+                                color: Colors.white,
+                                fontFamily: 'OpenSans'),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "App Name.com",
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.teal,
+                                fontFamily: 'OpenSans'),
+                          ),
+                        ]),
+                  )
+                ]),
               ),
-              const AppDetailsSlider(), 
+              const AppDetailsSlider(),
               Container(
                 height: 35,
                 margin: const EdgeInsets.symmetric(vertical: 10),
                 padding: const EdgeInsets.symmetric(horizontal: 30),
                 child: ElevatedButton(
-                  onPressed:() {
-                    
-                  },
+                  onPressed: () {},
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.teal,
-                textStyle:
-                    const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18),
-                    )
-                  ), 
+                      backgroundColor: Colors.teal,
+                      textStyle: const TextStyle(
+                          fontSize: 15, fontWeight: FontWeight.bold),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      )),
                   child: const Text('Coming Soon'),
                 ),
               ),
               Container(
                 height: 200,
-                child: CardDetailsSlider(),
+                child: const CardDetailsSlider(),
               ),
               Container(
                 height: 200,
-                padding: EdgeInsets.all(20),
+                padding: const EdgeInsets.all(20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("About this App", style: TextStyle(color: Colors.white, fontSize: 16, fontFamily: 'OpenSans', fontWeight: FontWeight.bold),),
-                    SizedBox(height: 10,),
-                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris imperdiet nisl at pellentesque facilisis. Sed condimentum risus id ipsum aliquet, vel vestibulum arcu consectetur. ", style: TextStyle(color: Colors.grey),),
-                    ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
+                    const Text(
+                      "About this App",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontFamily: 'OpenSans',
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris imperdiet nisl at pellentesque facilisis. Sed condimentum risus id ipsum aliquet, vel vestibulum arcu consectetur. ",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(3.5),
                             child: Container(
-                              color: Colors.green,
-                              height: 10,
-                            )
+                              margin: const EdgeInsets.only(right: 20),
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey)),
+                              height: 30,
+                              width: 80,
+                              child: const Center(
+                                  child: Text(
+                                "Test",
+                                style: TextStyle(color: Colors.grey),
+                              )),
+                            ),
                           ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(3.5),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey)),
+                              height: 30,
+                              width: 80,
+                              child: const Center(
+                                  child: Text(
+                                "Test",
+                                style: TextStyle(color: Colors.grey),
+                              )),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
                   ],
                 ),
               ),
+              Container(
+                height: 300,
+                child: Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(3.5),
+                    child: Container(
+                      margin: const EdgeInsets.only(right: 20),
+                      decoration:
+                          BoxDecoration(border: Border.all(color: Colors.grey)),
+                      height: 400,
+                      width: MediaQuery.of(context).size.width * .85,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Center(
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Row(
+                                  children: [
+                                    const Icon(
+                                      Icons.share_outlined,
+                                      color: Colors.grey,
+                                    ),
+                                    Container(
+                                      margin: const EdgeInsets.symmetric(
+                                          horizontal: 20),
+                                      child: Column(
+                                        children: [
+                                          const Text(
+                                              "No data shared with third parties"),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Icon(
+                                      Icons.cloud_download_outlined,
+                                      color: Colors.grey,
+                                    ),
+                                    Container(
+                                      margin: const EdgeInsets.symmetric(
+                                          horizontal: 20),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          const Text(
+                                              "This app may collect these data types"),
+                                          const SizedBox(height: 10),
+                                          const Text(
+                                              "Location, Personal info and 5 others"),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Icon(
+                                      Icons.lock_outline_rounded,
+                                      color: Colors.grey,
+                                    ),
+                                    Container(
+                                      margin: const EdgeInsets.symmetric(
+                                          horizontal: 20),
+                                      child: Column(
+                                        children: [
+                                          const Text("Data is encrypted"),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Icon(
+                                      Icons.delete_outline_rounded,
+                                      color: Colors.grey,
+                                    ),
+                                    Container(
+                                      margin: const EdgeInsets.symmetric(
+                                          horizontal: 20),
+                                      child: Column(
+                                        children: [
+                                          const Text(
+                                              "You can request that data to be deleted"),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ]),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+                child: Text(
+                  "All prices included VAT",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              )
             ]),
           ),
         ],
@@ -105,5 +258,3 @@ class _AppDetailsState extends State<AppDetails> {
     );
   }
 }
-
-
--- lib/widgets/app_details_slider.dart
+++ lib/widgets/app_details_slider.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppDetailsSlider extends StatelessWidget {
-
   const AppDetailsSlider({super.key});
 
   @override
@@ -9,15 +8,10 @@ class AppDetailsSlider extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: 75,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          
-          Expanded(
-            child: CustomDetailsCard()
-          ),
-          
-        ]),
+      child:
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: const [
+        Expanded(child: CustomDetailsCard()),
+      ]),
     );
   }
 }
@@ -25,167 +19,187 @@ class AppDetailsSlider extends StatelessWidget {
 class CustomDetailsCard extends StatelessWidget {
   const CustomDetailsCard({super.key});
 
-  
-  
-
   @override
   Widget build(BuildContext context) {
-   
-    return  ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                Container(
-      
-                  width: 150,
-                  child: Card(
-                    color: Colors.black,
-                    child: ClipPath(
-                      
-                      child: Container(
-                        padding: const EdgeInsets.all(16),
-                        decoration: const BoxDecoration(
-                          border: Border(
-                            right: BorderSide(color: Colors.greenAccent, width: .4,),
-                          ),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Text(
-                                  '4.5',
-                                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white),
-                                ),
-                                Icon(Icons.star, color: Colors.white, size: 15,)
-                              ],
-                            ),
-                            Row(
-                              children: const [
-                                  Text("100K reviews", style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.white),),
-                                  SizedBox(width: 2,),
-                                  Icon(Icons.info_outline_rounded, size: 14, color: Colors.white,)
-                              ],
-                              
-                            )
-                            
-                          ],
-                        ),
-                      ),
+    return ListView(scrollDirection: Axis.horizontal, children: [
+      Container(
+          width: 150,
+          child: Card(
+            color: Colors.black,
+            child: ClipPath(
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    right: BorderSide(
+                      color: Colors.greenAccent,
+                      width: .4,
                     ),
-                  )
+                  ),
                 ),
-                Container(
-      
-                  width: 150,
-                  child: Card(
-                    color: Colors.black,
-                    child: ClipPath(
-                      
-                      child: Container(
-                        padding: const EdgeInsets.all(16),
-                        decoration: const BoxDecoration(
-                          border: Border(
-                            right: BorderSide(color: Colors.greenAccent, width: .4,),
-                          ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          '4.5',
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
                         ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: const [
-                            Icon(Icons.download, size: 20, color: Colors.white,),
-                            SizedBox(width: 2,),
-                            Text("12MB", style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.white),),
-                          ],
-                        ),
-                      ),
+                        Icon(
+                          Icons.star,
+                          color: Colors.white,
+                          size: 15,
+                        )
+                      ],
                     ),
-                  )
+                    Row(
+                      children: const [
+                        Text(
+                          "100K reviews",
+                          style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        SizedBox(
+                          width: 2,
+                        ),
+                        Icon(
+                          Icons.info_outline_rounded,
+                          size: 14,
+                          color: Colors.white,
+                        )
+                      ],
+                    )
+                  ],
                 ),
-                Container(
-      
-                  width: 150,
-                  child: Card(
-                    color: Colors.black,
-                    child: ClipPath(
-                      
-                      child: Container(
-                        padding: const EdgeInsets.all(16),
-                        decoration: const BoxDecoration(
-                          border: Border(
-                            right: BorderSide(color: Colors.greenAccent, width: .4,),
-                          ),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: const [
-                            
-                            FadeInImage(
-                              placeholder: AssetImage('assets/no-image.jpg'),
-                              image: AssetImage("assets/E.png"),
-                              width: 35,
-                              height: 35,
-                              fit: BoxFit.cover,
-                            ),
-                           
-
-                          ],
-                        ),
-                      ),
-                      
+              ),
+            ),
+          )),
+      Container(
+          width: 150,
+          child: Card(
+            color: Colors.black,
+            child: ClipPath(
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    right: BorderSide(
+                      color: Colors.greenAccent,
+                      width: .4,
                     ),
-                  )
+                  ),
                 ),
-                Container(
-      
-                  width: 150,
-                  child: Card(
-                    color: Colors.black,
-                    child: ClipPath(
-                      
-                      child: Container(
-                        padding: const EdgeInsets.all(16),
-                        decoration: const BoxDecoration(
-                          border: Border(
-                            right: BorderSide(color: Colors.greenAccent, width: .4,),
-                          ),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Text(
-                                  '1M+',
-                                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white),
-                                ),
-                                
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                  Text("Downloads", style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.white),),
-                               
-                              ],
-                              
-                            )
-                            
-                          ],
-                        ),
-                      ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.download,
+                      size: 20,
+                      color: Colors.white,
                     ),
-                  )
+                    SizedBox(
+                      width: 2,
+                    ),
+                    Text(
+                      "12MB",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ],
                 ),
-
-                ]
-            );
-    
+              ),
+            ),
+          )),
+      Container(
+          width: 150,
+          child: Card(
+            color: Colors.black,
+            child: ClipPath(
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    right: BorderSide(
+                      color: Colors.greenAccent,
+                      width: .4,
+                    ),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    FadeInImage(
+                      placeholder: AssetImage('assets/no-image.jpg'),
+                      image: AssetImage("assets/E.png"),
+                      width: 35,
+                      height: 35,
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )),
+      Container(
+          width: 150,
+          child: Card(
+            color: Colors.black,
+            child: ClipPath(
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    right: BorderSide(
+                      color: Colors.greenAccent,
+                      width: .4,
+                    ),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          '1M+',
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          "Downloads",
+                          style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )),
+    ]);
   }
 }
-
